15b5b9dd46b92deb5cc956ed52c219f1e20b056f
corinnegottleb <corinneottlieb@gmail.com>
2018-12-06 18:19:57 +0200

    quiz2 notes

--- week2/answers.js
+++ week2/answers.js
@@ -4,18 +4,18 @@
 
 const answers = {
     1: "d",
-    2: "c",
+    2: "c", answer is b You must also use grid-area in the direct children elements
     3: "d",
     4: "c",
-    5: "a",
+    5: "a", arrow function is only for lexical function (where the function was invoked), since x.go() is global, the variable is undefined, which will still be invoked 1.5 seconds later
     6: "a",
     7: "a",
     8: "c",
-    9: "a",
-    10: "a",
+    9: "a",  c: "No, because `_memories` is outside of Brain", memories is global! if it was inside Brain function it would be closure becaus eclosure is when a function uses a variable from an outter function
+    10: "a", c: underscore means private
     11: "c",
     12: "b",
-    13: "c",
+    13: "c", must put a return or the function will return undefined
     14: "d",
     15: "a",
     16: "c",
